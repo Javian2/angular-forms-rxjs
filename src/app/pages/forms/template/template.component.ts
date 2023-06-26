b860c5fdd2409dde62d4c9ca6ca40a85e20132ad
Javian2 <52936547+Javian2@users.noreply.github.com>
2023-06-26 18:49:25 +0200

    Done template form

--- src/app/pages/forms/template/template.component.ts
+++ src/app/pages/forms/template/template.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+interface Hero {
+  name: string;
+  alter_ego: string;
+  power: PowerOptions;
+  enemies: number;
+  dead: boolean;
+}
+
+type PowerOptions = 'invisibilidad' | 'fuerza' | 'volar' | 'rayos' | 'fuego'
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
   styleUrls: ['./template.component.scss']
 })
+
 export class TemplateComponent {
+
+  constructor() { }
+
+  powerNames: string[] = ['invisibilidad', 'fuerza', 'fuego', 'rayos', 'volar']
+  heroModel: Hero = {
+    name: 'Superman',
+    alter_ego: 'Pepe',
+    power: 'volar',
+    enemies: 1,
+    dead: false
+  }
+
+  onSubmit(form: NgForm){
+    console.log('Propiedades de un formulario ngForm', form)
+    console.log('Valores de los campos del formulario', form.value)
+  }
+
+  updateFormValues(heroForm: NgForm){
+    heroForm.form.patchValue({
+      name: 'Batman',
+      alter_ego: 'Bruce',
+      dead: true
+    })
+  }
+
+  updateAllFormValues(heroForm: NgForm){
+    heroForm.form.setValue({
+      name: 'Spiderman',
+      alter_ego: 'Peter Parker',
+      power: 'fuerza',
+      enemies: 5,
+      dead: false
+    })
+  }
 
 }
--- src/app/pages/forms/template/template.module.ts
+++ src/app/pages/forms/template/template.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { TemplateComponent } from './template.component';
+import { FormsModule } from '@angular/forms';
 
 const routes: Routes = [
   { path: '', component: TemplateComponent },
@@ -12,6 +13,7 @@ const routes: Routes = [
     TemplateComponent
   ],
   imports: [
+    FormsModule,
     CommonModule,
     RouterModule.forChild(routes)
   ],
