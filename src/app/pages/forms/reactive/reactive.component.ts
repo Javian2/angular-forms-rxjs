487175cbd0298b555cdb4f2bd4f83d2448f2d342
Javian2 <52936547+Javian2@users.noreply.github.com>
2023-07-03 00:19:55 +0200

    Started reactive form

--- src/app/pages/forms/reactive/reactive.component.ts
+++ src/app/pages/forms/reactive/reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -6,5 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./reactive.component.scss']
 })
 export class ReactiveComponent {
+
+  constructor(private fb: FormBuilder) { }
+  
+  name = new FormControl('');
+  profileForm = this.fb.group({
+    firstName: ['Javi', Validators.required],
+    lastName: [''],
+    address: this.fb.group({
+      street: [''],
+      city: ['Novelda', Validators.minLength(5)],
+      state: [''],
+      zip: ['']
+    }),
+  })
+
+  // profileForm = new FormGroup({
+  //   firstName: new FormControl(''),
+  //   lastName: new FormControl(''),
+  //   address: new FormGroup({
+  //     street: new FormControl('calle'),
+  //     city: new FormControl(''),
+  //     state: new FormControl(''),
+  //     zip: new FormControl('')
+  //   })
+  // });
+
+
+  updateName(){
+    this.name.setValue('Pepe')
+  }
+
+  updateProfile(){
+    this.profileForm.patchValue({
+      firstName: 'Jaime',
+      address: {
+        street: '123 Drew Street'
+      }
+    });
+  }
+
+  onSubmit() {
+    console.log(this.profileForm.value);
+  }
 
 }
--- src/app/pages/forms/reactive/reactive.module.ts
+++ src/app/pages/forms/reactive/reactive.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ReactiveComponent } from './reactive.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
   { path: '', component: ReactiveComponent},
@@ -13,6 +14,7 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     RouterModule.forChild(routes)
   ],
   exports: [
